15b9ad783f97db0d72953145126af908042fc3c3
Xotabich <mitanoow@gmail.com>
2026-06-27 21:08:21 +0200

    worked abit about world_gen.c

--- src/client/core/world_gen.c
+++ src/client/core/world_gen.c
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "core/light.h"
 
 static pthread_t workers[GEN_WORKERS];
@@ -11,17 +12,16 @@ static pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t work_cond = PTHREAD_COND_INITIALIZER;
 
 static WorkJob job_queue[WORK_QUEUE_CAP];
-static int job_head;
-static int job_tail;
-static int job_count;
+static int job_head = 0, job_tail = 0, job_count = 0;
 
-static WorkResult result_queue[WORK_QUEUE_CAP];
-static int result_head;
-static int result_tail;
-static int result_count;
+static WorkResult gen_result_queue[WORK_QUEUE_CAP];
+static int gen_head = 0, gen_tail = 0, gen_count = 0;
 
-static int work_running;
-static int work_shutdown;
+static WorkResult mesh_result_queue[WORK_QUEUE_CAP];
+static int mesh_head = 0, mesh_tail = 0, mesh_count = 0;
+
+static int work_running = 0;
+static int work_shutdown = 0;
 
 static void queue_push_job(WorkJob job) {
     job_queue[job_tail] = job;
@@ -30,26 +30,38 @@ static void queue_push_job(WorkJob job) {
 }
 
 static int queue_pop_job(WorkJob* out) {
-    if (job_count <= 0)
-        return 0;
+    if (job_count <= 0) return 0;
     *out = job_queue[job_head];
     job_head = (job_head + 1) % WORK_QUEUE_CAP;
     job_count--;
     return 1;
 }
 
-static void queue_push_result(WorkResult res) {
-    result_queue[result_tail] = res;
-    result_tail = (result_tail + 1) % WORK_QUEUE_CAP;
-    result_count++;
+static void push_gen_result(WorkResult res) {
+    gen_result_queue[gen_tail] = res;
+    gen_tail = (gen_tail + 1) % WORK_QUEUE_CAP;
+    gen_count++;
 }
 
-static int queue_pop_result(WorkResult* out) {
-    if (result_count <= 0)
-        return 0;
-    *out = result_queue[result_head];
-    result_head = (result_head + 1) % WORK_QUEUE_CAP;
-    result_count--;
+static int pop_gen_result(WorkResult* out) {
+    if (gen_count <= 0) return 0;
+    *out = gen_result_queue[gen_head];
+    gen_head = (gen_head + 1) % WORK_QUEUE_CAP;
+    gen_count--;
+    return 1;
+}
+
+static void push_mesh_result(WorkResult res) {
+    mesh_result_queue[mesh_tail] = res;
+    mesh_tail = (mesh_tail + 1) % WORK_QUEUE_CAP;
+    mesh_count++;
+}
+
+static int pop_mesh_result(WorkResult* out) {
+    if (mesh_count <= 0) return 0;
+    *out = mesh_result_queue[mesh_head];
+    mesh_head = (mesh_head + 1) % WORK_QUEUE_CAP;
+    mesh_count--;
     return 1;
 }
 
@@ -63,18 +75,28 @@ static int coords_in_job_queue(int cx, int cz, JobKind kind) {
     return 0;
 }
 
-static int coords_in_result_queue(int cx, int cz, JobKind kind) {
-    for (int i = 0; i < result_count; i++) {
-        int idx = (result_head + i) % WORK_QUEUE_CAP;
-        if (result_queue[idx].kind == kind &&
-            result_queue[idx].cx == cx && result_queue[idx].cz == cz)
+static int coords_in_gen_result(int cx, int cz) {
+    for (int i = 0; i < gen_count; i++) {
+        int idx = (gen_head + i) % WORK_QUEUE_CAP;
+        if (gen_result_queue[idx].cx == cx && gen_result_queue[idx].cz == cz)
+            return 1;
+    }
+    return 0;
+}
+
+static int coords_in_mesh_result(int cx, int cz) {
+    for (int i = 0; i < mesh_count; i++) {
+        int idx = (mesh_head + i) % WORK_QUEUE_CAP;
+        if (mesh_result_queue[idx].cx == cx && mesh_result_queue[idx].cz == cz)
             return 1;
     }
     return 0;
 }
 
 static int coords_in_flight(int cx, int cz, JobKind kind) {
-    return coords_in_job_queue(cx, cz, kind) || coords_in_result_queue(cx, cz, kind);
+    if (coords_in_job_queue(cx, cz, kind)) return 1;
+    if (kind == JOB_GEN) return coords_in_gen_result(cx, cz);
+    else return coords_in_mesh_result(cx, cz);
 }
 
 static void discard_job(WorkJob* job) {
@@ -83,10 +105,11 @@ static void discard_job(WorkJob* job) {
 }
 
 static void discard_result(WorkResult* res) {
-    if (res->kind == JOB_GEN)
-        free(res->gen_data);
-    else
-        chunk_mesh_result_free(res->mesh);
+    if (res->kind == JOB_GEN) {
+        if (res->gen_data) free(res->gen_data);
+    } else {
+        if (res->mesh) chunk_mesh_result_free(res->mesh);
+    }
 }
 
 static void* worker_main(void* arg) {
@@ -111,12 +134,13 @@ static void* worker_main(void* arg) {
         res.cx = job.cx;
         res.cz = job.cz;
         res.kind = job.kind;
+        res.gen_data = NULL;
+        res.mesh = NULL;
 
         if (job.kind == JOB_GEN) {
             Chunk chunk;
             chunk_generate(&chunk, job.cx, job.cz);
             res.gen_data = chunk.data;
-            res.mesh = NULL;
         } else {
             ChunkMeshResult* mesh = (ChunkMeshResult*)calloc(1, sizeof(ChunkMeshResult));
             uint32_t gen = job.mesh->generation;
@@ -138,14 +162,20 @@ static void* worker_main(void* arg) {
             free(job.mesh);
             mesh->generation = gen;
             res.mesh = mesh;
-            res.gen_data = NULL;
         }
 
         pthread_mutex_lock(&queue_mutex);
-        if (result_count < WORK_QUEUE_CAP)
-            queue_push_result(res);
-        else
-            discard_result(&res);
+        if (res.kind == JOB_GEN) {
+            if (gen_count < WORK_QUEUE_CAP)
+                push_gen_result(res);
+            else
+                discard_result(&res);
+        } else {
+            if (mesh_count < WORK_QUEUE_CAP)
+                push_mesh_result(res);
+            else
+                discard_result(&res);
+        }
         pthread_mutex_unlock(&queue_mutex);
     }
 
@@ -153,11 +183,11 @@ static void* worker_main(void* arg) {
 }
 
 void world_gen_start(void) {
-    if (work_running)
-        return;
+    if (work_running) return;
 
     job_head = job_tail = job_count = 0;
-    result_head = result_tail = result_count = 0;
+    gen_head = gen_tail = gen_count = 0;
+    mesh_head = mesh_tail = mesh_count = 0;
     work_shutdown = 0;
     work_running = 1;
 
@@ -166,8 +196,7 @@ void world_gen_start(void) {
 }
 
 void world_gen_stop(void) {
-    if (!work_running)
-        return;
+    if (!work_running) return;
 
     pthread_mutex_lock(&queue_mutex);
     work_shutdown = 1;
@@ -183,9 +212,14 @@ void world_gen_stop(void) {
         queue_pop_job(&job);
         discard_job(&job);
     }
-    while (result_count > 0) {
+    while (gen_count > 0) {
         WorkResult res;
-        queue_pop_result(&res);
+        pop_gen_result(&res);
+        discard_result(&res);
+    }
+    while (mesh_count > 0) {
+        WorkResult res;
+        pop_mesh_result(&res);
         discard_result(&res);
     }
     pthread_mutex_unlock(&queue_mutex);
@@ -216,27 +250,18 @@ int world_gen_submit(int cx, int cz) {
     return ok;
 }
 
-// pls work
 int world_gen_poll(int* cx, int* cz, uint16_t** data) {
     int ok = 0;
-    WorkResult deferred[WORK_QUEUE_CAP];
-    int deferred_count = 0;
 
     pthread_mutex_lock(&queue_mutex);
-    while (result_count > 0) {
+    if (gen_count > 0) {
         WorkResult res;
-        queue_pop_result(&res);
-        if (!ok && res.kind == JOB_GEN) {
-            *cx = res.cx;
-            *cz = res.cz;
-            *data = res.gen_data;
-            ok = 1;
-        } else {
-            deferred[deferred_count++] = res;
-        }
+        pop_gen_result(&res);
+        *cx = res.cx;
+        *cz = res.cz;
+        *data = res.gen_data;
+        ok = 1;
     }
-    for (int i = 0; i < deferred_count; i++)
-        queue_push_result(deferred[i]);
     pthread_mutex_unlock(&queue_mutex);
 
     return ok;
@@ -249,7 +274,7 @@ int world_mesh_in_flight(int cx, int cz) {
     return found;
 }
 
-int world_mesh_submit(World* world, int cx, int cz) {
+int world_mesh_submit(struct World* world, int cx, int cz) {
     Chunk* chunk = world_get_chunk(world, cx, cz);
     if (!chunk || !chunk->data)
         return 0;
@@ -279,25 +304,17 @@ int world_mesh_submit(World* world, int cx, int cz) {
 
 int world_mesh_poll(int* cx, int* cz, ChunkMeshResult** mesh, uint32_t* generation) {
     int ok = 0;
-    WorkResult deferred[WORK_QUEUE_CAP];
-    int deferred_count = 0;
 
     pthread_mutex_lock(&queue_mutex);
-    while (result_count > 0) {
+    if (mesh_count > 0) {
         WorkResult res;
-        queue_pop_result(&res);
-        if (!ok && res.kind == JOB_MESH) {
-            *cx = res.cx;
-            *cz = res.cz;
-            *mesh = res.mesh;
-            *generation = res.mesh->generation;
-            ok = 1;
-        } else {
-            deferred[deferred_count++] = res;
-        }
+        pop_mesh_result(&res);
+        *cx = res.cx;
+        *cz = res.cz;
+        *mesh = res.mesh;
+        *generation = res.mesh->generation;
+        ok = 1;
     }
-    for (int i = 0; i < deferred_count; i++)
-        queue_push_result(deferred[i]);
     pthread_mutex_unlock(&queue_mutex);
 
     return ok;
